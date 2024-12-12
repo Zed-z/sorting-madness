0b9e088dfb4e69b83c81907c641c4294830e3e6b
Zed-z <71879051+Zed-z@users.noreply.github.com>
2024-12-12 21:04:21 +0100

    Merge sort

---
+++ src/main/java/pl/put/poznan/transformer/logic/SortingStrategyMergeSort.java
@@ -0,0 +1,54 @@
+package pl.put.poznan.transformer.logic;
+
+public class SortingStrategyMergeSort implements SortingStrategy {
+    public SortingStrategyMergeSort() {
+        super();
+    }
+
+    @Override
+    public void sort(SortedObject[] objects, Comparator comparator) {
+        int n = objects.length;
+        mergeSort(objects, n, comparator);
+    }
+
+    public static void mergeSort(SortedObject[] objects, int n, Comparator comparator) {
+        if (n < 2) {
+            return;
+        }
+        int mid = n / 2;
+        SortedObject[] l = new SortedObject[mid];
+        SortedObject[] r = new SortedObject[n - mid];
+
+        for (int i = 0; i < mid; i++) {
+            l[i] = objects[i];
+        }
+        for (int i = mid; i < n; i++) {
+            r[i - mid] = objects[i];
+        }
+        mergeSort(l, mid, comparator);
+        mergeSort(r, n - mid, comparator);
+
+        merge(objects, l, r, mid, n - mid, comparator);
+    }
+
+    public static void merge(
+            SortedObject[] objects, SortedObject[] l, SortedObject[] r, int left, int right, Comparator comparator) {
+
+        int i = 0, j = 0, k = 0;
+        while (i < left && j < right) {
+            if (comparator.compare(l[i], r[j])) {
+                objects[k++] = l[i++];
+            }
+            else {
+                objects[k++] = r[j++];
+            }
+        }
+        while (i < left) {
+            objects[k++] = l[i++];
+        }
+        while (j < right) {
+            objects[k++] = r[j++];
+        }
+    }
+
+}
