e46a909511d20974e30f5b66bbbbdd48f908d555
Zed-z <71879051+Zed-z@users.noreply.github.com>
2024-12-14 23:26:01 +0100

    Bubblesort tests and bugfixes

--- src/main/java/pl/put/poznan/transformer/logic/SortingStrategyBubbleSort.java
+++ src/main/java/pl/put/poznan/transformer/logic/SortingStrategyBubbleSort.java
@@ -7,12 +7,12 @@ public class SortingStrategyBubbleSort implements SortingStrategy {
 
     @Override
     public void sort(SortedObject[] objects, Comparator comparator) {
-        for (int i = 0; i < objects.length - 1; i++) {
-            for (int j = i + 1; j < objects.length; j++) {
-                if (comparator.compare(objects[i], objects[j]) ) {
+        for (int i = 0; i < objects.length; i++) {
+            for (int j = 0; j < objects.length; j++) {
+                if (comparator.compare(objects[j], objects[i])) {
                     SortedObject temp = objects[j];
-                    objects[j] = objects[j + 1];
-                    objects[j + 1] = temp;
+                    objects[j] = objects[i];
+                    objects[i] = temp;
                 }
             }
         }
